77ef01650817ad664059b5901665d3b1ebc04dd4
kalaiselvaraj <kalaiselvaraj02@gmail.com>
2026-06-12 20:19:25 +0530

    Add files via upload

---
+++ inter_day2_ (3).py
@@ -0,0 +1,20 @@
+
+A="kalai"
+B="Vani"
+print(A+B)
+print(len(A))
+print(len(B))
+
+list
+list1=[30,28,48]
+list2=["geeks","for","geeks"]
+print("In list of total numbers:",list1)
+print("In list items:",list2)
+
+tuple
+Student_Names=["Kaviya","Mohana","karthi"]
+print(Student_Names)
+
+dict
+student={"name":"kamal","age":14}
+print(student)
