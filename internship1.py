0c7f58abb8af21e6349ced30eac0cb69c417480e
kalaiselvaraj <kalaiselvaraj02@gmail.com>
2026-06-12 20:18:45 +0530

    Add files via upload

---
+++ internship1.py
@@ -0,0 +1,17 @@
+Name="kalai"
+Age=18
+CGPA=6.9
+Placed=False
+print("Student Name:",Name)
+print("Age:",Age)
+print("CGPA:",CGPA)
+print("Placed:",Placed)
+print(type(Name))
+print(type(Age))
+print(type(CGPA))
+print(type(Placed))
+
+
+
+
+
